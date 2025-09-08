ee712dc3778b3de5fd9ea846ea952a6a639006df
Thiago Panini <panini.development@gmail.com>
2025-09-07 22:11:41 -0300

    feat: including s3, iam and lambda infra definitions

---
+++ infra/lambda.tf
@@ -0,0 +1,70 @@
+/* -----------------------------------------------------------------------------
+FILE: lambda.tf
+
+DESCRIPTION:
+  This Terraform configuration uses a remote module to provision an AWS Lambda
+  function for processing B3 active tickers. The function's name, runtime,
+  handler, and environment variables are parameterized via input variables for
+  flexible deployments.
+
+RESOURCES:
+  - module.aws_lambda_function:
+    Deploys a Lambda function using the tfbox module with configurable properties
+    such as name, runtime, handler, and environment variables.
+----------------------------------------------------------------------------- */
+
+/* --------------------------------------------------------
+   LAMBDA FUNCTION: get-investment-portfolio
+   Defines the AWS Lambda Function for getting data from
+   an user's investment portfolio source
+-------------------------------------------------------- */
+
+module "iam_role_lambda_get_investment_portfolio" {
+  source = "git::https://github.com/ThiagoPanini/tfbox.git?ref=aws/iam-role/v0.2.0"
+
+  role_name                   = "role-b3stocks-get-investment-portfolio"
+  trust_policy_filepath       = "${path.module}/assets/iam/trust_policies/trust-lambda.json"
+  policy_templates_source_dir = "${path.module}/assets/iam/policy_templates"
+
+  policy_templates_vars = {
+    "region_name" = local.region_name,
+    "account_id"  = local.account_id,
+    "bucket_name" = local.s3_artifacts_bucket_name,
+    "object_key"  = var.s3_investment_portfolio_object_key
+  }
+}
+
+
+module "lambda_function_get_investment_portfolio" {
+  source = "git::https://github.com/ThiagoPanini/tfbox.git?ref=aws/lambda-function/v0.3.1"
+
+  function_name = "b3stocks-get-investment-portfolio"
+  runtime       = "python3.12"
+  timeout       = 180
+
+  role_arn = module.iam_role_lambda_get_investment_portfolio.role_arn
+
+  source_code_path = "../app"
+  lambda_handler   = "app.src.features.get_investment_portfolio.presentation.get_investment_portfolio_presentation.handler"
+
+  environment_variables = {
+    S3_B3STOCKS_ARTIFACT_BUCKET_NAME   = local.s3_artifacts_bucket_name
+    S3_INVESTMENT_PORTFOLIO_OBJECT_KEY = var.s3_investment_portfolio_object_key
+  }
+
+  create_lambda_layers = true
+  layers_map = {
+    pynamodb_6_1_0 = {
+      requirements = [
+        "pynamodb==6.1.0",
+      ]
+      runtime     = "python3.12"
+      description = "Dependencies for getting an user's investment portfolio from a given source"
+    }
+  }
+
+  create_eventbridge_trigger = true
+  cron_expression            = var.lambda_function_cron_expression
+
+  tags = var.tags
+}
---
+++ infra/s3_buckets.tf
@@ -0,0 +1,37 @@
+/* -----------------------------------------------------------------------------
+  FILE: s3.tf
+
+  DESCRIPTION:
+    This Terraform file provisions all S3 buckets required by all app features
+    on b3stocks project.
+
+  RESOURCES:
+    - aws_s3_bucket.artifacts:
+        Creates an S3 bucket for storing artifacts on b3stocks project
+
+    - aws_s3_bucket_public_access_block.artifacts_bucket_public_access_block:
+        Blocks all public access to the artifacts S3 bucket.
+----------------------------------------------------------------------------- */
+
+/* --------------------------------------------------------
+   S3 BUCKET: Artifacts
+   A S3 bucket for storing artifacts on b3stocks project.
+-------------------------------------------------------- */
+
+resource "aws_s3_bucket" "artifacts" {
+  bucket = local.s3_artifacts_bucket_name
+
+  tags = var.tags
+}
+
+resource "aws_s3_bucket_public_access_block" "artifacts_bucket_public_access_block" {
+  bucket                  = aws_s3_bucket.artifacts.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  depends_on = [
+    aws_s3_bucket.artifacts
+  ]
+}
