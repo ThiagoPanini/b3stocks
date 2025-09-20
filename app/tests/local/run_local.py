71bb363adf968af6b94fbfa34feeb342d2c74f63
Thiago Panini <panini.development@gmail.com>
2025-09-20 19:42:17 -0300

    feat: adding handler for fundamentus eod stock metrics feature

--- app/tests/local/run_local.py
+++ app/tests/local/run_local.py
@@ -27,7 +27,7 @@ DESCRIPTION:
     This feature provides functionality retrieve investment portfolio data from
     a given source defined by users through a adapter (e.g., S3, Google Drive, APIs).
 """
-# response = get_investment_portfolios_handler(event=None, context=None)
+response = get_investment_portfolios_handler(event=None, context=None)
 # print(response)
 
 
@@ -37,7 +37,7 @@ FEATURE: Store DynamoDB Streams Data
 DESCRIPTION:
     This feature provides functionality to stream data from DynamoDB and process it in real-time.
 """
-# response = store_dynamodb_streams_data_handler(event=MOCKED_DYNAMODB_STREAMS_EVENT, context=None)
+response = store_dynamodb_streams_data_handler(event=MOCKED_DYNAMODB_STREAMS_EVENT, context=None)
 # print(response)
 
 
@@ -48,7 +48,7 @@ DESCRIPTION:
     This feature provides functionality to scrape and retrieve active stock data from
     a web site (e.g., Fundamentus) and process it for further use.
 """
-# response = get_active_stocks_handler(event=None, context=None)
+response = get_active_stocks_handler(event=None, context=None)
 # print(response)
 
 
@@ -60,4 +60,4 @@ DESCRIPTION:
     the Fundamentus investment website and process it for further use.
 """
 response = get_fundamentus_eod_stock_metrics_handler(event=MOCKED_SQS_EVENT, context=None)
-print(response)
+# print(response)
